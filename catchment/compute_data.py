16d53fb97610a4ec30e93186345a39542a559f36
Hannah Little <hanlit@ceh.ac.uk>
2024-03-20 14:32:47 +0000

    refactoring and abstracting

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -15,20 +15,25 @@ def analyse_data(data_dir):
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
+    data_fls = load_catchment_data(data_dir)
+    return compute_standard_deviation_by_day(data_fls)
+    
+
+
+def compute_standard_deviation_by_day(data):
+    daily_std_list = map(daily_std, data)
+    daily_standard_deviation = pd.concat(daily_std_list)
+    
+    return daily_standard_deviation
+
+
+def daily_std(dataset):
+    return dataset.groupby(dataset.index.date).std()
+    
+
+def load_catchment_data(data_dir):
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
-
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
-
-    graph_data = {
-        'daily standard deviation': daily_standard_deviation
-    }
-
-    views.visualize(graph_data)
+    return list(data)
