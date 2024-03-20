74990a67596f6767a8685ef409f2f63914a87001
stehee <stehee@ceh.ac.uk>
2024-03-20 14:34:56 +0000

    Edited compute_data

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -7,6 +7,31 @@ import pandas as pd
 
 from catchment import models, views
 
+def daily_std(data):
+    return data.groupby(data.index.date).std()
+
+def compute_standard_deviation_by_day(data):
+   daily_std_list = map(daily_std, data)
+   daily_standard_deviation = pd.concat(daily_std_list)
+   return daily_standard_deviation
+
+# def compute_standard_deviation_by_day(data):
+#    daily_std_list = []
+#    for dataset in data:
+#        daily_std = dataset.groupby(dataset.index.date).std()
+#        daily_std_list.append(daily_std)
+
+#    daily_standard_deviation = pd.concat(daily_std_list)
+#    return daily_standard_deviation
+
+
+def load_catchment_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError('No CSV files found in the data directory')
+    data = map(models.read_variable_from_csv, data_file_paths)
+    return list(data)
+
 
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
@@ -15,16 +40,8 @@ def analyse_data(data_dir):
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+    data = load_catchment_data(data_dir)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
+    daily_standard_devaition = compute_standard_deviation_by_day(data)
 
-    return daily_standard_deviation
+    return compute_standard_deviation_by_day(data)
