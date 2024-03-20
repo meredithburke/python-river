744884447fce687a86ac78ebdcb4bc7376a25b8d
Stephen Pearson <s.pearson@bristol.ac.uk>
2024-03-20 16:18:09 +0000

    Updating code to accept JSON files and introducing classes

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -17,18 +17,6 @@ def compute_standard_deviation_by_day(data):
     return daily_standard_deviation
 
 
-def load_catchment_data(data_dir):
-    """
-    """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    
-    data = map(models.read_variable_from_csv, data_file_paths)
-
-    return data
-
-
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
@@ -36,8 +24,34 @@ def analyse_data(data_dir):
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data = load_catchment_data(data_dir)
-    
+    data = data_dir.load_catchment_data()
+
     daily_standard_deviation = compute_standard_deviation_by_day(data)
 
     return daily_standard_deviation
+
+
+class CSVDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        
+        data = map(models.read_variable_from_csv, data_file_paths)
+
+        return list(data)
+
+
+class JSONDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No JSON files found in the data directory')
+        
+        data = map(models.read_variable_from_json, data_file_paths)
+
+        return list(data)
