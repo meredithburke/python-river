c4bad96b73a99e0af19384fc9cda3f41556068db
Meredith <meredith.burke@live.ca>
2024-03-21 10:46:28 +0000

    dodge fix to date strings

--- catchment/models.py
+++ catchment/models.py
@@ -23,7 +23,8 @@ def read_variable_from_csv(filename, measurements='Rainfall (mm)'):
     dataset = pd.read_csv(filename, usecols=['Date', 'Site', measurements])
 
     dataset = dataset.rename({'Date':'OldDate'}, axis='columns')
-    dataset['Date'] = [pd.to_datetime(x,dayfirst=True) for x in dataset['OldDate']]
+    dataset['Date'] = [
+        pd.to_datetime(x,dayfirst=True, format='mixed') for x in dataset['OldDate']]
     dataset = dataset.drop('OldDate', axis='columns')
 
     newdataset = pd.DataFrame(index=dataset['Date'].unique())
