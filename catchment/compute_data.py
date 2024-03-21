2215cdc951aca09b02c4a7880e98172d47afbf30
Hannah Little <hanlit@ceh.ac.uk>
2024-03-21 11:35:18 +0000

    compute data add

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,14 +8,14 @@ import pandas as pd
 from catchment import models, views
 
 
-def analyse_data(data_dir):
+def analyse_data(data_source):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data_fls = load_catchment_data(data_dir)
+    data_fls = data_source.load_catchment_data()
     return compute_standard_deviation_by_day(data_fls)
     
 
@@ -37,3 +37,27 @@ def load_catchment_data(data_dir):
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
     return list(data)
+
+class CSVDataSource:
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        data = map(models.read_variable_from_csv, data_file_paths)
+        return list(data)
+    
+class JSONDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No JSON files found in the data directory')
+        data = map(models.read_variable_from_json, data_file_paths)
+        return list(data)
+
